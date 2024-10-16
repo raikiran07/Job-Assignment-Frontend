e873642ce354122ee4889c7b26f22ef0cc947024
raikiran07 <raikiran925@gmail.com>
2024-10-16 19:30:39 +0530

    usercontexprovided path fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import UserContextProvider from './userContext/userContext.jsx'
+import UserContextProvider from './userContext/UserContext.jsx'
 
 createRoot(document.getElementById('root')).render(
  
